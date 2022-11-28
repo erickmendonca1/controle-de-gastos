edb7f0506318d5e46e741a70bf35f26b1c1ecb78
Erick Mendonça <erickm.uff@gmail.com>
2022-11-28 20:43:26 -0300

    Mensagens de erros para login e senha sendo validadas

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-
-
-
-function validateFields(){
-
-    const emailValid = isEmailValid();
-    document.getElementById("recover-password-button").disabled = !emailValid;
-
-    const passwordValid = isPasswordValid();
-    document.getElementById("login-button").disabled = !emailValid || !passwordValid;
-
+function onChangeEmail(){
+    toggleButtonDisable();
+    toggleEmailErros();  
+    
+    
 }
+
+function onChangePassword(){
+    toggleButtonDisable();
+    togglePasswordErrors();
+}
+
 
 function isEmailValid() {
     const email = document.getElementById("email").value;
@@ -30,4 +30,41 @@ function isPasswordValid() {
 
 function validateEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
+}
+
+function toggleEmailErros(){
+
+    const email = document.getElementById("email").value;
+
+    if(!email){
+        document.getElementById("email-required-error").style.display = "block";
+    } else{
+        document.getElementById("email-required-error").style.display = "none";
+    }
+
+    if(validateEmail(email)){
+        document.getElementById("email-invalid-error").style.display = "none";
+    } else {
+        document.getElementById("email-invalid-error").style.display = "block";
+    }
+
+
+}
+
+function toggleButtonDisable(){
+    const emailValid = isEmailValid();
+    document.getElementById("recover-password-button").disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById("login-button").disabled = !emailValid || !passwordValid;
+
+}
+
+function togglePasswordErrors(){
+    const password = document.getElementById("password").value;
+    if (!password){
+        document.getElementById("password-required-error").style.display = "block";
+    } else{
+        document.getElementById("password-required-error").style.display = "none";
+    }
 }
