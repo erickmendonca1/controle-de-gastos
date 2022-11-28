2820e5f558cef4e341a9fdaba4464e6675ed89a6
Erick Mendonça <erickm.uff@gmail.com>
2022-11-28 18:40:04 -0300

    Conserto do bug que impedia o botão entrar de funcionar

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
+
+
+
 function validateFields(){
-    const email = document.getElementById("email").value //pegar o valor do campo de email
-    if(!email){
-        document.getElementById("recover-password-button").disabled = true;// verificar se o email não é vazio e se o email é válido
-    // se verdadeiro, então habilitar o botão recuperar senha
-    // se falso, desabilitar o botão recuperar senha
-    } else if (validateEmail(email)){
-        document.getElementById("revover-password-button").disabled = false;
 
-    }else{
+    const emailValid = isEmailValid();
+    document.getElementById("recover-password-button").disabled = !emailValid;
 
-    }
+    const passwordValid = isPasswordValid();
+    document.getElementById("login-button").disabled = !emailValid || !passwordValid;
 
 }
 
-function validateEmail(email){
+function isEmailValid() {
+    const email = document.getElementById("email").value;
+    if (!email) {
+        return false;
+    }
+    return validateEmail(email);
+}
+
+
+function isPasswordValid() {
+    const password = document.getElementById("password").value;
+    if (!password) {
+        return false;
+    }
+    return true;
+}
+
+function validateEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
 }
-
